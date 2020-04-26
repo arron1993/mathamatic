616ffc75ad29cf7c93bf7935319810b0db88a764
Arron Moore <arron.moore@appliansys.com>
2020-04-26 23:32:13 +0100

    added difficulty reveal animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
@@ -29,6 +31,7 @@ import { TopicDifficultyPipe } from './pipes/topic-difficulty.pipe';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule
   ],
--- src/app/components/topic-list/topic-list.component.ts
+++ src/app/components/topic-list/topic-list.component.ts
@@ -1,18 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { TopicService } from '../../services/topic.service';
+import {
+  trigger,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
-  selector: 'app-topic-list',
-  templateUrl: './topic-list.component.html',
-  styleUrls: ['./topic-list.component.scss']
+    selector: 'app-topic-list',
+    templateUrl: './topic-list.component.html',
+    styleUrls: ['./topic-list.component.scss'],
+    animations: [
+        trigger('difficultySlide', [
+            transition(':enter', [
+                style({transform: 'translateY(-200%)'}),
+                animate('400ms ease-in', style({transform: 'translateY(0%)'}))
+            ]),
+            transition(':leave', [
+                animate('0ms ease-in', style({transform: 'translateY(-100%)'}))
+            ])
+        ])
+    ]
 })
 
 export class TopicListComponent implements OnInit {
     topics = [];
-
+    selectedTopic;
     constructor(private ts: TopicService) { }
 
     ngOnInit() {
         this.topics = this.ts.getTopics();
+    }
+
+    selectTopic(topic) {
+        this.selectedTopic = this.selectedTopic !== topic ? topic : undefined;
     }
 }
